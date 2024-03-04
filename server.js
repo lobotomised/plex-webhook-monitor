1359b72508546626caee3a18e086473acc4b7bd4
lobotomised <34192441+lobotomised@users.noreply.github.com>
2024-03-04 20:00:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const fs = require('fs/promises');
 const multer = require('multer');
+const path = require('path');
 
 const app = express();
-const upload = multer({dest: '/tmp/'});
+
+var upload = multer({ dest: '/tmp/' });
+
+app.get('/', (request, response) => {
+    console.log('get request')
+
+    response.sendStatus(200)
+});
 
 app.post('/', upload.single('thumb'), async (request, response) => {
     const payload = JSON.parse(request.body.payload);
+    console.log('Got webhook for', payload);
 
-    await fs.appendFile('payload.txt', JSON.stringify(payload + '\n'));
+
+    const currentDirectory = __dirname;
+    const filePath = path.join(currentDirectory, 'payload.txt');
+
+    await fs.appendFile(filePath, JSON.stringify(payload) + '\n');
 
     response.sendStatus(200);
 });
 
-app.listen(10000)
+app.listen(10000);
 
